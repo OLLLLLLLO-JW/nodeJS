69a457de83c28986d00f63e70f3f8a320a2fe391
Jesse Weis <jesselweis@gmail.com>
2024-02-15 20:21:16 -0700

    small update

--- assertionTesting/assRoutes.js
+++ assertionTesting/assRoutes.js
@@ -1,7 +1,6 @@
 import { getAllTheData } from "./assController.js";
 const routes = (app) =>{
     app.route("/assertionTestOne/:paramOne").get((req, res, next) => {
-        // res.send("test Responseflksl");
         next();
     }, getAllTheData);
 }
